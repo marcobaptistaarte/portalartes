bfa99fead73a630539e839c5a77965404b2d7ddc
marcobaptistaarte <marco.bap@gmail.com>
2025-12-22 20:59:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,46 @@
 
-const CACHE_NAME = 'portal-artes-cache-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'portal-artes-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/index.tsx'
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache aberto');
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  // Ignorar requisições de API e extensões
+  if (event.request.url.startsWith('http')) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      }).catch(() => {
+        return caches.match('/');
+      })
+    );
+  }
 });
